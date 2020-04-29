bdc23ce3b292d0f8d12ba51273ad2e384ce88910
Isadora Haas Hillebrand <isadorahaas@gmail.com>
2020-04-29 23:23:53 +0100

    beach detail api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.scss";
-import axios from "axios";
-import BeachDetail from "./components/BeachDetail";
-import Home from "./components/Home";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import axios from 'axios';
+import BeachDetail from './components/BeachDetail';
+import Home from './components/Home';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,19 +18,19 @@ class App extends React.Component {
   }
 
   getBeaches() {
-    const apiKey = "key=xaJX84ccYJ93CJwroxGVNlxvHshmxJVV";
-    const showRegions = "list?show=regions";
-    const apiCountry = "country=PT";
-    const apiBeaches = "category=beach";
-    const apiLimit = "limit=50";
-    const apiNearby = "nearby={lat},{lng},{radius}";
-    const apiExclude = "exclude={webcamid}[,{webcamid}...]";
-    const apiPopularity = "orderby=popularity";
-    const apiShowImageLocation = "?show=webcams:location,image";
+    const apiKey = 'key=xaJX84ccYJ93CJwroxGVNlxvHshmxJVV';
+    //const showRegions = 'list?show=regions';
+    const apiCountry = 'country=PT';
+    const apiBeaches = 'category=beach';
+    const apiLimit = 'limit=50';
+    //const apiNearby = 'nearby={lat},{lng},{radius}';
+    //const apiExclude = 'exclude={webcamid}[,{webcamid}...]';
+    const apiPopularity = 'orderby=popularity';
+    //const apiShowImageLocation = '?show=webcams:location,image';
     const url = `https://api.windy.com/api/webcams/v2/list/${apiCountry}/${apiLimit}/${apiBeaches}/${apiPopularity}?show=webcams:image,location,player&${apiKey}`;
 
     axios.get(url).then((response) => {
-      console.log(response.data.result.webcams[0]);
+      //console.log(response.data.result.webcams);
       this.setState({
         beaches: response.data.result.webcams,
       });
@@ -38,15 +38,19 @@ class App extends React.Component {
   }
 
   render() {
+    const { beaches } = this.state;
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <Home apiBeaches={this.state.beaches} />
+            <Home apiBeaches={beaches} />
           </Route>
-          <Route path="/beach/:beachId">
-            <BeachDetail />
-          </Route>
+          <Route
+            path="/beach/:beachId"
+            component={(props) => (
+              <BeachDetail {...props} apiBeaches={beaches} />
+            )}
+          />
         </Switch>
       </Router>
     );
--- src/components/BeachDetail.js
+++ src/components/BeachDetail.js
@@ -1,14 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const BeachDetail = ({ player }) => {
-  return (
-    <div>
-      <Link to={`../`}>Homepage</Link>
-      <p>Detaillll</p>
-      <iframe width="300" height="200" src={player}></iframe>
-    </div>
-  );
-};
+class BeachDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      status: '',
+      title: '',
+      locationCity: '',
+      imageCurrentPreview: '',
+      playerLifetimeEmbed: '',
+    };
+  }
+
+  getBeach = (id) => {
+    const apiBeaches = this.props.apiBeaches;
+    const beach = apiBeaches.find((apiBeach) => apiBeach.id === id);
+    this.setState({
+      id: beach.id,
+      status: beach.status,
+      title: beach.title,
+      locationCity: beach.location.city,
+      imageCurrentPreview: beach.image.current.preview,
+      playerLifetimeEmbed: beach.player.lifetime.embed,
+    });
+  };
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { beachId },
+      },
+    } = this.props;
+    this.getBeach(beachId);
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      match: {
+        params: { beachId },
+      },
+    } = this.props;
+
+    const prevBeachId = prevProps.match.params.beachId;
+    if (prevBeachId !== beachId) {
+      this.getBeach(beachId);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to={`../`}>Homepage</Link>
+        <h1></h1>
+        <iframe
+          width="300"
+          height="200"
+          src={this.state.playerLifetimeEmbed}
+        ></iframe>
+      </div>
+    );
+  }
+}
 
 export default BeachDetail;
