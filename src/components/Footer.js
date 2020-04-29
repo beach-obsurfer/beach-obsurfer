d8b034b9e4a4c081add2edc90074c0c96bc72a82
Isadora Haas Hillebrand <isadorahaas@gmail.com>
2020-04-30 00:15:28 +0100

    Footer setted up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,40 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="footer">
-      <div>
-        <span>Created by </span>
-        <a href="https://github.com/Arnaud75020">Arnaud</a>
-        <span> | </span>
-        <a href="https://github.com/filipamarta">Filipa</a>
-        <span> | </span>
-        <a href="https://github.com/isadorahaashillebrand">Isadora</a>
-        <span> - </span>
-        <a href="https://www.wildcodeschool.com">Wild Code School</a>
-      </div>
-    </div>
+    <footer>
+      Created by
+      <a
+        href="https://github.com/Arnaud75020"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Arnaud Duhé
+      </a>
+      <span> | </span>
+      <a
+        href="https://github.com/filipamarta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Filipa Marta
+      </a>
+      <span> | </span>
+      <a
+        href="https://github.com/isadorahaashillebrand"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Isadora Hillebrand
+      </a>
+      <span> - </span>
+      <a
+        href="https://www.wildcodeschool.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wild Code School
+      </a>
+    </footer>
   );
 }
 
