ba12574c4cf3147ed067c3ecb2b1ea598dba3a94
Isadora Haas Hillebrand <isadorahaas@gmail.com>
2020-04-29 23:51:10 +0100

    footer - setted up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 
-const Footer = () => {
-  return <div>....</div>;
-};
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <span>Created by </span>
+        <a href="https://github.com/Arnaud75020">Arnaud</a>
+        <span> | </span>
+        <a href="https://github.com/filipamarta">Filipa</a>
+        <span> | </span>
+        <a href="https://github.com/isadorahaashillebrand">Isadora</a>
+        <span> - </span>
+        <a href="https://www.wildcodeschool.com">Wild Code School</a>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
