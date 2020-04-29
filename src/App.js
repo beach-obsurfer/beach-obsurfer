1bbcad932d88c4481a1887233b5e1505469d9f9b
Arnaud Duhé <anro010@hotmail.com>
2020-04-29 15:10:53 +0100

    boiler plate completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Spinner from 'react-bootstrap/Spinner';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import App from './App';
+import './App.scss';
 import * as serviceWorker from './serviceWorker';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
