dca75b1c0a34f919e89588f48af4331a3285f20c
Filipa Marta <filipa.marta@gmail.com>
2020-04-30 13:07:33 +0100

    loader structure and style

--- src/components/BeachDetail.js
+++ src/components/BeachDetail.js
@@ -11,6 +11,7 @@ import coffeeicon from "../image/coffee-blue.svg";
 import peticon from "../image/pet-blue.svg";
 import logo from "../image/logotipo.svg";
 import wavesicon from "../image/waves-orange.svg";
+import Loader from "./Loader";
 
 class BeachDetail extends React.Component {
   constructor(props) {
@@ -62,114 +63,125 @@ class BeachDetail extends React.Component {
   }
 
   render() {
+    const { isPageLoaded } = this.props;
     return (
       <div className="beach-detail">
-        <Container fluid>
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={12}>
-              <header>
-                <Row>
-                  <Col xs={12} sm={12} md={12} lg={12}>
-                    <Link to={`../`} className="back-btn">
-                      <img src={logo} alt="logo" className="logo" />
-                      <div>
-                        <img
-                          src={arrowlefticon}
-                          alt="button to go back to the homepage"
-                          className="arrow-left-icon"
-                        />
-                        Go back to the Homepage
-                      </div>
-                    </Link>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col
-                    xs={12}
-                    sm={12}
-                    md={{ span: 10, offset: 1 }}
-                    lg={{ span: 8, offset: 2 }}
-                  >
-                    <h1>{this.state.title}</h1>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col
-                    className="text-center"
-                    xs={12}
-                    sm={12}
-                    md={12}
-                    lg={{ span: 10, offset: 1 }}
-                  >
-                    <ul className="icons-wrapper">
-                      <li>
-                        <img src={localizationicon} alt="localization icon" />
-                        {this.state.locationCity}
-                      </li>
-                      <li>
-                        <img src={surficon} alt="Surf" />
-                        Surf spot
-                      </li>
-                      <li>
-                        <img src={beachicon} alt="Beach" />
-                        Beach spot
-                      </li>
-                      <li>
-                        <img src={peticon} alt="Pet" />
-                        Pet Friendly spot
-                      </li>
-                      <li>
-                        <img src={coffeeicon} alt="Coffee" />
-                        Food places
-                      </li>
-                    </ul>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
-                    <img
-                      src={wavesicon}
-                      alt="waves icon"
-                      className="waves-icon"
-                    />
-                  </Col>
-                </Row>
-              </header>
-            </Col>
-          </Row>
-          <Row>
-            <Col
-              xs={12}
-              sm={{ span: 10, offset: 1 }}
-              md={{ span: 8, offset: 2 }}
-              lg={{ span: 6, offset: 3 }}
-            >
-              <p>
-                Portugal is famed for its beautiful beaches, and along the 1,794
-                km of coastline are found some of Europe’s finest beaches. These
-                beaches are as diverse and varied as Portugal, ranging from vast
-                expanses of golden sands through to secluded coves with
-                picturesque scenery. If you are seeking a country that offers
-                beautiful beaches and a glorious climate, then Portugal is the
-                ideal holiday destination for you.
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
-              <iframe
-                width="90%"
-                height="500px"
-                src={this.state.playerLifetimeEmbed}
-              ></iframe>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={12}>
-              <Footer />
-            </Col>
-          </Row>
-        </Container>
+        {isPageLoaded ? (
+          <Container fluid>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <header>
+                  <Row>
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                      <Link to={`../`} className="back-btn">
+                        <img src={logo} alt="logo" className="logo" />
+                        <div>
+                          <img
+                            src={arrowlefticon}
+                            alt="button to go back to the homepage"
+                            className="arrow-left-icon"
+                          />
+                          Go back to the Homepage
+                        </div>
+                      </Link>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col
+                      xs={12}
+                      sm={12}
+                      md={{ span: 10, offset: 1 }}
+                      lg={{ span: 8, offset: 2 }}
+                    >
+                      <h1>{this.state.title}</h1>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col
+                      className="text-center"
+                      xs={12}
+                      sm={12}
+                      md={12}
+                      lg={{ span: 10, offset: 1 }}
+                    >
+                      <ul className="icons-wrapper">
+                        <li>
+                          <img src={localizationicon} alt="localization icon" />
+                          {this.state.locationCity}
+                        </li>
+                        <li>
+                          <img src={surficon} alt="Surf" />
+                          Surf spot
+                        </li>
+                        <li>
+                          <img src={beachicon} alt="Beach" />
+                          Beach spot
+                        </li>
+                        <li>
+                          <img src={peticon} alt="Pet" />
+                          Pet Friendly spot
+                        </li>
+                        <li>
+                          <img src={coffeeicon} alt="Coffee" />
+                          Food places
+                        </li>
+                      </ul>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col
+                      className="text-center"
+                      xs={12}
+                      sm={12}
+                      md={12}
+                      lg={12}
+                    >
+                      <img
+                        src={wavesicon}
+                        alt="waves icon"
+                        className="waves-icon"
+                      />
+                    </Col>
+                  </Row>
+                </header>
+              </Col>
+            </Row>
+            <Row>
+              <Col
+                xs={12}
+                sm={{ span: 10, offset: 1 }}
+                md={{ span: 8, offset: 2 }}
+                lg={{ span: 6, offset: 3 }}
+              >
+                <p>
+                  Portugal is famed for its beautiful beaches, and along the
+                  1,794 km of coastline are found some of Europe’s finest
+                  beaches. These beaches are as diverse and varied as Portugal,
+                  ranging from vast expanses of golden sands through to secluded
+                  coves with picturesque scenery. If you are seeking a country
+                  that offers beautiful beaches and a glorious climate, then
+                  Portugal is the ideal holiday destination for you.
+                </p>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
+                <iframe
+                  width="90%"
+                  height="500px"
+                  src={this.state.playerLifetimeEmbed}
+                ></iframe>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <Footer />
+              </Col>
+            </Row>
+          </Container>
+        ) : (
+          <Loader />
+        )}
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import logo from "../image/logotipo.svg";
 import "./Home.scss";
 import SearchBar from "./SearchBar";
 import ModalPopup from "./ModalPopup";
+import Loader from "./Loader";
 
 class Home extends React.Component {
   constructor(props) {
@@ -16,87 +17,93 @@ class Home extends React.Component {
     };
   }
 
-  handleModal = () => {     
-      const { isPopupShowing } = this.state;     
-      this.setState({ isPopupShowing: !isPopupShowing });   
-    };
+  handleModal = () => {
+    const { isPopupShowing } = this.state;
+    this.setState({ isPopupShowing: !isPopupShowing });
+  };
 
   handleChange = (event) => {
     this.setState({ searchBeachValue: event.target.value });
   };
 
   handleSubmit = () => {
-
     let searchBeachValue = this.state.searchBeachValue;
 
-     let beaches = this.props.apiBeaches;
+    let beaches = this.props.apiBeaches;
 
-        beaches = beaches.filter((beach) => {
-            if (
-                beach.location.city.toLowerCase() === searchBeachValue.toLowerCase() ||
-                beach.title.toLowerCase().includes(searchBeachValue.toLowerCase())
-              ) {
-                return beach;
-                } 
-              }
-        )
-        console.log(beaches)
+    beaches = beaches.filter((beach) => {
+      if (
+        beach.location.city.toLowerCase() === searchBeachValue.toLowerCase() ||
+        beach.title.toLowerCase().includes(searchBeachValue.toLowerCase())
+      ) {
+        return beach;
+      }
+    });
+    console.log(beaches);
 
-        if (!beaches.length) {
-            this.handleModal();         
-        } else { 
-            this.props.updateBeachHandler(beaches);
-        }
+    if (!beaches.length) {
+      this.handleModal();
+    } else {
+      this.props.updateBeachHandler(beaches);
+    }
   };
 
   render() {
-    const { apiBeaches } = this.props;
+    const { apiBeaches, isPageLoaded } = this.props;
     return (
       <div className="homepage">
-        <Container fluid>
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={12}>
-              <header>
-                <img src={logo} alt="logo" className="logo" />
-                <h1>~ Find the best beaches in Portugal! ~</h1>
-              </header>
-            </Col>
-          </Row>
-          <Row>
-            <Col
-            className="text-center"
-              xs={12}
-              sm={{ span: 10, offset: 1 }}
-              md={{ span: 8, offset: 2 }}
-              lg={{ span: 6, offset: 3 }}
-            >
-              <SearchBar
-                input={this.state.searchInputValue}
-                inputChangeHandler={this.handleChange}
-                inputSubmitHandler={this.handleSubmit}
-              />
-            <ModalPopup city={this.state.searchBeachValue} show={this.state.isPopupShowing} handlemodal={this.handleModal} />
-            </Col>
-          </Row>
-          <Row>
-            {apiBeaches.map((beach) => (
-              <Col xs={12} sm={6} md={6} lg={4}>
-                <Link
-                  to={`/beach/${beach.id}`}
-                  id={`beach-${beach.id}`}
-                  className="card-wrapper"
-                >
-                  <Beach
-                    key={beach.id}
-                    title={beach.title}
-                    image={beach.image.current.preview}
-                    player={beach.player.lifetime.embed}
-                  />
-                </Link>
+        {isPageLoaded ? (
+          <Container fluid>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <header>
+                  <img src={logo} alt="logo" className="logo" />
+                  <h1>~ Find the best beaches in Portugal! ~</h1>
+                </header>
               </Col>
-            ))}
-          </Row>
-        </Container>
+            </Row>
+            <Row>
+              <Col
+                className="text-center"
+                xs={12}
+                sm={{ span: 10, offset: 1 }}
+                md={{ span: 8, offset: 2 }}
+                lg={{ span: 6, offset: 3 }}
+              >
+                <SearchBar
+                  input={this.state.searchInputValue}
+                  inputChangeHandler={this.handleChange}
+                  inputSubmitHandler={this.handleSubmit}
+                />
+                <ModalPopup
+                  city={this.state.searchBeachValue}
+                  show={this.state.isPopupShowing}
+                  handlemodal={this.handleModal}
+                />
+              </Col>
+            </Row>
+            <Row>
+              {apiBeaches.map((beach) => (
+                <Col xs={12} sm={6} md={6} lg={4}>
+                  <Link
+                    to={`/beach/${beach.id}`}
+                    id={`beach-${beach.id}`}
+                    className="card-wrapper"
+                  >
+                    <Beach
+                      key={beach.id}
+                      title={beach.title}
+                      image={beach.image.current.preview}
+                      player={beach.player.lifetime.embed}
+                    />
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        ) : (
+          <Loader />
+        )}
       </div>
     );
   }
